c306383c6337427af8388bd922e5eeaf515b8805
Karel Profant <Karel.Profant@seznam.cz>
2025-08-26 19:46:07 +0200

    modified:   cypress-tests/cypress.config.ts

--- cypress-tests/cypress.config.ts
+++ cypress-tests/cypress.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "oxj3dr",
     watchForFileChanges: false,
     baseUrl: "https://obchod.24u.cz/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      //CYPRESS_RECORD_KEY a6002f34-3629-44a0-ac9a-71546454227d
+      //npx cypress run --record --key a6002f34-3629-44a0-ac9a-7154645422
     },
   },
 });
+
+
