adcf783aaec74d6e957bd046588ca40a257739ed
Karel Profant <Karel.Profant@seznam.cz>
2025-08-28 19:40:49 +0200

    oprava 6

--- cypress-tests/cypress/e2e/categoryPage.cy.ts
+++ cypress-tests/cypress/e2e/categoryPage.cy.ts
@@ -9,7 +9,7 @@ import { Countries } from "../pages/registrationPage";
 describe('Testy na vytváření objednávek ze stránky kategorie', () => {
     beforeEach(() => {
         // Arrange
-        cy.visit('/', {timeout: 60000})
+        cy.visit('/')
         cy.get('a[class*="CaSaveAll"]').click({ force: true })
         // Act
         menuComponent.openCategoryPage(menuCategories.IPHONE)
@@ -33,7 +33,7 @@ describe('Testy na vytváření objednávek ze stránky kategorie', () => {
             cy.intercept('GET', /https:\/\/obchod.24u.cz\/(.*)\?do=addCart/).as('addCart')
             // Act
             item.addItemToCart()
-            cy.wait('@addCart', { timeout: 60000 }).then((odpoved) => {
+            cy.wait('@addCart', { timeout: 120000 }).then((odpoved) => {
                 // Asset
                 cartPage.getAddedToCartDialog()
                     .should('exist')
@@ -60,7 +60,7 @@ describe('Testy na vytváření objednávek ze stránky kategorie', () => {
                 .and('be.visible')
                 .and('contain.text', data.itemToCart.name)
             productDetailPage.addItemToCart()
-            cy.wait('@addCart', { timeout: 60000 }).then((odpoved) => {
+            cy.wait('@addCart', { timeout: 120000 }).then((odpoved) => {
                 cartPage.getAddedToCartDialog()
                     .should('exist')
                     .should('be.visible')
@@ -89,7 +89,7 @@ describe('Testy na vytváření objednávek ze stránky kategorie', () => {
             cy.intercept('GET', /https:\/\/obchod.24u.cz\/(.*)\?do=addCart/).as('addCart')
             // Act
             item.addItemToCart()
-            cy.wait('@addCart', { timeout: 60000 }).then((odpoved) => {
+            cy.wait('@addCart', { timeout: 120000 }).then((odpoved) => {
                 // Act
                 cartPage.getToCartFromDialogButton().click()
                 // Asset
